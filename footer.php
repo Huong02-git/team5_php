8522d5d52807714d5a6e548cc1050ac933f54d13
Quynhtrang363 <132880147+Quynhtrang363@users.noreply.github.com>
2023-08-12 22:45:45 +0700

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,37 @@
+<footer class="footer">
+
+   <section class="box-container">
+
+      <div class="box">
+         <h3>quick links</h3>
+         <a href="home.php"> <i class="fas fa-angle-right"></i> home</a>
+         <a href="dishes.php"> <i class="fas fa-angle-right"></i> shop</a>
+         <a href="category.php"> <i class="fas fa-angle-right"></i> category</a>
+      </div>
+
+      <div class="box">
+         <h3>extra links</h3>
+         <a href="cart.php"> <i class="fas fa-angle-right"></i> cart</a>
+         <a href="login.php"> <i class="fas fa-angle-right"></i> login</a>
+         <a href="register.php"> <i class="fas fa-angle-right"></i> register</a>
+      </div>
+
+      <div class="box">
+         <h3>contact info</h3>
+         <p> <i class="fas fa-phone"></i> 0949853835 </p>
+         <p> <i class="fas fa-envelope"></i> admin01@gmail.com </p>
+         <p> <i class="fas fa-map-marker-alt"></i> Đại học Thương mại </p>
+      </div>
+
+      <div class="box">
+         <h3>follow us</h3>
+         <a href="#"> <i class="fab fa-facebook-f"></i> facebook </a>
+         <a href="#"> <i class="fab fa-twitter"></i> twitter </a>
+         <a href="#"> <i class="fab fa-instagram"></i> instagram </a>
+      </div>
+
+   </section>
+
+   <p class="credit"> @ <?= date('Y'); ?> by <span>VANTH</span></p>
+
+</footer>
