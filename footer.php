844ecfe68c7d372df881dd55c0825c2e7cb37929
anh0112 <luongmap1102@gmail.com>
2023-08-16 01:03:17 +0700

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,37 @@
+<footer class="footer">
+
+   <section class="box-container">
+
+      <div class="box">
+         <h3>quick links</h3>
+         <a href="home.php"> <i class="fas fa-angle-right"></i> Home</a>
+         <a href="dishes.php"> <i class="fas fa-angle-right"></i> Order</a>
+         <a href="category.php"> <i class="fas fa-angle-right"></i> Category</a>
+      </div>
+
+      <div class="box">
+         <h3>extra links</h3>
+         <a href="cart.php"> <i class="fas fa-angle-right"></i> Cart</a>
+         <a href="login.php"> <i class="fas fa-angle-right"></i> Login</a>
+         <a href="register.php"> <i class="fas fa-angle-right"></i> Register</a>
+      </div>
+
+      <div class="box">
+         <h3>contact info</h3>
+         <p> <i class="fas fa-phone"></i> 0949853835 </p>
+         <p> <i class="fas fa-envelope"></i> admin01@gmail.com </p>
+         <p> <i class="fas fa-map-marker-alt"></i> Đại học Thương mại </p>
+      </div>
+
+      <div class="box">
+         <h3>follow us</h3>
+         <a href="#"> <i class="fab fa-facebook-f"></i> Facebook </a>
+         <a href="#"> <i class="fab fa-twitter"></i> Twitter </a>
+         <a href="#"> <i class="fab fa-instagram"></i> Instagram </a>
+      </div>
+
+   </section>
+
+   <p class="credit"> @ <?= date('Y'); ?> by <span>VANTH</span></p>
+
+</footer>
