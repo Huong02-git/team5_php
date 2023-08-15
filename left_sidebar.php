080cd5fd1d1d6ed528a018e21fd8e175c6f29ff9
khanhvinguyen <132880086+khanhvinguyen@users.noreply.github.com>
2023-08-15 21:17:45 +0700

    Create left_sidebar.php

---
+++ left_sidebar.php
@@ -0,0 +1,57 @@
+
+<div id="main-wrapper">
+  <!-- Left Sidebar  -->
+  <div class="left-sidebar">
+            <!-- Sidebar scroll-->
+            <div class="scroll-sidebar">
+                <!-- Sidebar navigation-->
+                <nav class="sidebar-nav">
+                   <ul id="sidebarnav">
+                        <li class="nav-devider"></li>
+                        <li class="nav-label">Admin Panel</li>
+                        <li> <a class="has-arrow  " href="#" aria-expanded="false"><i class="fa fa-tachometer"></i><span class="hide-menu">Home</span></a>
+                            <ul aria-expanded="false" class="collapse">
+                                <li><a href="admin_page.php">Dashboard</a></li>
+                                
+                            </ul>
+                        </li>
+                        <li class="nav-label">Options</li>
+                        <li> <a class="has-arrow  " href="#" aria-expanded="false">  <span><i class="fa fa-user f-s-20 "></i></span><span class="hide-menu">Users</span></a>
+                            <ul aria-expanded="false" class="collapse">
+                                <li><a href="admin_users.php">All Users</a></li>
+							
+								
+                               
+                            </ul>
+                        </li>
+                        <li> <a class="has-arrow  " href="#" aria-expanded="false"><i class="fa fa-archive f-s-20 color-warning"></i><span class="hide-menu">Menu</span></a>
+                            <ul aria-expanded="false" class="collapse">
+								<li><a href="view_category.php">All Menu</a></li>
+								<li><a href="admin_categories.php">Add Menu</a></li>
+                    
+                                
+                            </ul>
+                        </li>
+                      <li> <a class="has-arrow  " href="#" aria-expanded="false"><i class="fa fa-cutlery" aria-hidden="true"></i><span class="hide-menu">Dishes</span></a>
+                            <ul aria-expanded="false" class="collapse">
+								<li><a href="view_dishes.php">All Dishes</a></li>
+								<li><a href="admin_dishes.php">Add Dish</a></li>
+                              
+                                
+                            </ul>
+                        </li>
+						 <li> <a class="has-arrow  " href="#" aria-expanded="false"><i class="fa fa-shopping-cart" aria-hidden="true"></i><span class="hide-menu">Orders</span></a>
+                            <ul aria-expanded="false" class="collapse">
+								<li><a href="admin_orders.php">All Orders</a></li>
+								  
+                            </ul>
+                        </li>
+                         
+                    </ul>
+                </nav>
+                <!-- End Sidebar navigation -->
+            </div>
+            <!-- End Sidebar scroll-->
+        </div>
+        <!-- End Left Sidebar  -->
+        </div>
