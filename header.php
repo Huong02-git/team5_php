da5af09455040600214f915a690e4ff5b13267f8
anh0112 <luongmap1102@gmail.com>
2023-08-16 01:03:13 +0700

    Create header.php

---
+++ header.php
@@ -0,0 +1,56 @@
+
+<header class="header">
+
+   <div class="flex">
+
+      <a href="home.php" class="logo"><i class="fa fa-cutlery" aria-hidden="true"></i>  Andee<span>.res</span></a>
+      <nav class="navbar">
+         <a href="home.php">Home</a>
+         <a href="category.php">Menu</a>
+        <a href="dishes.php">Dishes</a>
+         <a href="orders.php">Orders</a>
+      </nav>
+
+      <div class="icons">
+         <?php
+            $count_cart_items = $conn->prepare("SELECT * FROM cart WHERE user_id = ?");
+            $count_cart_items->execute([$user_id]);
+            $total_cart_counts = $count_cart_items->rowCount();
+         ?>
+         <div id="menu-btn" class="fas fa-bars"></div>
+         <div id="user-btn" class="fas fa-user"></div>
+         <a href="search_page.php" class="fas fa-search"></a>
+         <a href="cart.php"><i class="fas fa-shopping-cart"></i><span>(<?= $count_cart_items->rowCount(); ?>)</span></a>
+      </div>
+
+      <div class="profile">
+         <?php
+            $select_profile = $conn->prepare("SELECT * FROM `users` WHERE id = ?");
+            $select_profile->execute([$user_id]);
+            if($select_profile->rowCount() > 0){
+            $fetch_profile = $select_profile->fetch(PDO::FETCH_ASSOC);
+         ?>
+         <img src="uploaded_img/<?= $fetch_profile['image']; ?>" alt="">
+         <p><?= $fetch_profile['name']; ?></p>
+         <a href="user_profile_update.php" class="btn">Update profile</a>
+         <a href="logout.php" class="delete-btn">Logout</a>
+         <div class="flex-btn">
+            <a href="login.php" class="option-btn">Login</a>
+            <a href="register.php" class="option-btn">Register</a>
+         </div>
+         <?php
+            }else{
+         ?>
+         <p>Please Login or Register first!</p>
+         <div class="flex-btn">
+            <a href="user_register.php" class="option-btn">Register</a>
+            <a href="user_login.php" class="option-btn">Login</a>
+         </div>
+         <?php
+            }
+            ?>
+      </div>
+
+   </div>
+
+</header>
